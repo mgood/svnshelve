36bbde65c05b1430f5513f3a4e7193e83b2b2494
Matt Good <matt@matt-good.net>
2007-08-23 14:47:07 -0700

    add --display option to just print the contents of a shelf
    darcs-hash:20070823214707-25259-a3a777983f5b4084ed33476780ae03a2f0926888.gz

--- svnshelve/hunk_selector.py
+++ svnshelve/hunk_selector.py
@@ -5,7 +5,7 @@ import sys
 try:
     import pygments
 except ImportError:
-    def _print_patch(*parts):
+    def print_patch(*parts):
         print patch
 else:
     from pygments.formatters import TerminalFormatter
@@ -13,7 +13,7 @@ else:
     from pygments.lexers import get_lexer_by_name
     diff_lexer = get_lexer_by_name('diff')
     term_fmter = TerminalFormatter()
-    def _print_patch(*parts):
+    def print_patch(*parts):
         for p in parts:
             highlight(p, diff_lexer, term_fmter, sys.stdout)
 
@@ -67,7 +67,7 @@ class HunkSelector:
             while i < len(patch.hunks):
                 hunk = patch.hunks[i]
                 if lasti != i:
-                    _print_patch(patch.get_header(), hunk)
+                    print_patch(patch.get_header(), hunk)
                     j += 1
                 lasti = i
 
